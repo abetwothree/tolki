2af2789f6ffe62c5a9ec0b5ef2c6470ad778b544
Abraham Arango <skat_et_dieu@yahoo.com>
2025-09-03 07:26:43 -0500

    Format

--- packages/Str/vite.config.ts
+++ packages/Str/vite.config.ts
@@ -17,11 +17,7 @@ export default defineConfig({
             fileName: "str",
         },
         rollupOptions: {
-            external: [
-                "any-ascii",
-                "lodash-es",
-                "transliteration",
-            ],
+            external: ["any-ascii", "lodash-es", "transliteration"],
         },
     },
 });
