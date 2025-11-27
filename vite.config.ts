ec090b7813aa45f22a8d139162a38febc5787401
Abraham Arango <skat_et_dieu@yahoo.com>
2025-11-27 11:30:56 -0600

    Update coverage thresholds in Vite config
    Adjusted the coverage thresholds for statements, branches, functions, and lines to reflect updated test coverage metrics.

--- vite.config.ts
+++ vite.config.ts
@@ -62,10 +62,10 @@ export default defineConfig({
             exclude: [...configDefaults.exclude, "./scripts/**", "./docs/**"],
             thresholds: {
                 autoUpdate: true,
-                statements: 94.25,
+                statements: 94.32,
                 branches: 87.12,
-                functions: 97.4,
-                lines: 94.57,
+                functions: 97.79,
+                lines: 94.61,
             },
         },
         projects: ["packages/*"],
