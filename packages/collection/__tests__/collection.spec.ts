84328e4604dabea18a9510f78aedb9347e8f8a24
abetwothree <1899636+abetwothree@users.noreply.github.com>
2026-01-28 05:20:30 +0000

    Fix linting and formatting

--- packages/collection/__tests__/collection.spec.ts
+++ packages/collection/__tests__/collection.spec.ts
@@ -6166,7 +6166,12 @@ describe("Collection", () => {
                     }),
                 ).toBe(15);
 
-                const d = collect([{ foo: 10 }, { foo: 20 }, { foo: null }, {foo: 'house'}]);
+                const d = collect([
+                    { foo: 10 },
+                    { foo: 20 },
+                    { foo: null },
+                    { foo: "house" },
+                ]);
                 expect(
                     d.average((item) => {
                         return item.foo;
