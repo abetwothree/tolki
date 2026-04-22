a7f4a1258abfc2207e8f8e5a93a1319f5ad14a8f
Abraham Arango <skat_et_dieu@yahoo.com>
2026-04-22 07:56:19 -0500

    Update routes.spec.ts

--- packages/ts/__tests__/routes.spec.ts
+++ packages/ts/__tests__/routes.spec.ts
@@ -1,4 +1,5 @@
 import * as Ts from "@tolki/ts";
+import type { RouteQueryOptions } from "@tolki/types";
 import { afterEach, describe, expect, it } from "vitest";
 
 import * as Stubs from "./stubs";
